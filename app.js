38739220215b74c0388934de97b421a75debc13e
Musaib <atomicsight809@gmail.com>
2020-05-26 23:37:26 +0530

    Added a spinner for improved user experience

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // UI Variables
-const btn = document.querySelector('.btn');
+const btn = document.querySelector('.submitBtn');
 const result = document.querySelector('.output');
+const spinner = document.querySelector('#spinner')
 
 // Event Handler
 btn.addEventListener('click', fetchQuote);
@@ -8,12 +9,45 @@ btn.addEventListener('click', fetchQuote);
 // Fetch Quote
 async function fetchQuote() {
 
-  const response = await fetch('https://api.quotable.io/random');
+  showSpinner()
+  const response = await fetch('https://api.quotable.io/random').catch(err => {
+    hideSpinner();
+    showAlert('Please check your internet connection');
+  })
 
-  const data = await response.json();
+  if (response.ok) {
+    const data = await response.json();
+    hideSpinner();
+    result.innerHTML = `<h5>${data.content}</h5>`;
+    result.innerHTML += `<h2>${data.author}</h2>`;
 
-  result.innerHTML = `<h5>${data.content}</h5>`;
-  result.innerHTML += `<h2>${data.author}</h2>`;
+    result.classList.add('final');
+  }
+}
 
-  result.classList.add('final');
+// Alert Function
+function showAlert(message) {
+  const errorDiv = document.createElement("div");
+
+  errorDiv.className = "alert";
+
+  errorDiv.appendChild(document.createTextNode(message));
+
+  document.querySelector('.container').insertAdjacentElement('afterend', errorDiv)
+
+  setTimeout(() => {
+    document.querySelector(".alert").remove();
+  }, 2500);
+}
+
+// Show Spinner
+function showSpinner() {
+  spinner.style.display = 'block';
+  result.style.display = 'none';
+}
+
+// Hide Spinner
+function hideSpinner() {
+  spinner.style.display = 'none';
+  result.style.display = 'block'
 }
